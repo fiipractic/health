30601800729040a27f0ea0339d1310f446a9b352
Catalin Ciobanu <catalin.ciobanu@tss-yonder.com>
2015-03-21 15:33:11 +0200

    Basi dao layer

---
+++ src/main/java/ro/fiipractic/health/dao/PersonDAO.java
@@ -0,0 +1,104 @@
+package ro.fiipractic.health.dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import ro.fiipractic.health.domain.Person;
+import ro.fiipractic.health.util.HibernateUtil;
+
+public class PersonDAO {
+	private Session session;
+
+	/**
+	 * Insert a new Person object into the database.
+	 * 
+	 * @param prs
+	 *            the person to be saved.
+	 * @return true if the person has been successfully saved, false otherwise.
+	 */
+	public boolean savePerson(Person prs) {
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		try {
+			return session.save(prs) != null;
+		} catch (HibernateException e) {
+			System.err.println(e);
+			return false;
+		} finally {
+			session.getTransaction().commit();
+			session.close();
+		}
+	}
+
+	/**
+	 * Return a Person from database based on its id.
+	 * 
+	 * @param id
+	 *            the id from the database.
+	 * @return the person having the provided id.
+	 */
+	public Person getPersonByID(long id) {
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		Person prs = null;
+		try {
+			String hql = "FROM Person where id=" + id;
+			Query query = session.createQuery(hql);
+			List<Person> results = query.list();
+			prs = results.get(0);
+		} catch (HibernateException e) {
+			System.err.println(e);
+		} finally {
+			session.getTransaction().commit();
+			session.close();
+		}
+		return prs;
+	}
+
+	/**
+	 * Delete a Person
+	 * 
+	 * @param prs
+	 * @return true if the person was successfully removed from the database,
+	 *         false otherwise.
+	 */
+	public boolean deletePerson(Person prs) {
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		try {
+			session.delete(prs);
+			return true;
+		} catch (HibernateException e) {
+			System.err.println(e);
+		} finally {
+			session.getTransaction().commit();
+			session.close();
+		}
+		return false;
+	}
+
+	/**
+	 * Updates a person to a new state.
+	 * 
+	 * @param prs
+	 *            the new state of the Person object
+	 * @return the new state of the Person object
+	 */
+	public Person updatePerson(Person prs) {
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		try {
+
+			return (Person) session.merge(prs);
+		} catch (HibernateException e) {
+			System.err.println(e);
+		} finally {
+			session.getTransaction().commit();
+			session.close();
+		}
+		return null;
+	}
+}
--- src/main/java/ro/fiipractic/health/main/HibernateTest.java
+++ src/main/java/ro/fiipractic/health/main/HibernateTest.java
@@ -1,16 +1,20 @@
 package ro.fiipractic.health.main;
 
-import org.hibernate.Session;
-
+import ro.fiipractic.health.dao.PersonDAO;
 import ro.fiipractic.health.domain.Person;
-import ro.fiipractic.health.util.HibernateUtil;
 
 public class HibernateTest {
+	private static PersonDAO prsDAO;
+
 	public static void main(String[] args) {
 
-		Session session = HibernateUtil.getSessionFactory().openSession();
+		prsDAO = new PersonDAO();
+		insertMyPersons();
+		printFirstNames();
 
-		session.beginTransaction();
+	}
+
+	private static void insertMyPersons() {
 
 		Person person1 = new Person("Vasile", "Popescu");
 		person1.setCnp("173753839585");
@@ -21,12 +25,36 @@ public class HibernateTest {
 		Person person4 = new Person("Smaranda", "Gheorghiu");
 		person4.setCnp("273753839585");
 
-		session.save(person1);
-		session.save(person2);
-		session.save(person3);
-		session.save(person4);
-		
-		session.getTransaction().commit();
-		session.close();
+		if (prsDAO.savePerson(person1)) {
+			System.out.println("Person with cnp: " + person1.getCnp()
+					+ " successfully saved!");
+		}
+
+		if (prsDAO.savePerson(person2)) {
+			System.out.println("Person with cnp: " + person2.getCnp()
+					+ " successfully saved!");
+		}
+
+		if (prsDAO.savePerson(person3)) {
+			System.out.println("Person with cnp: " + person3.getCnp()
+					+ " successfully saved!");
+		}
+
+		if (prsDAO.savePerson(person4)) {
+			System.out.println("Person with cnp: " + person4.getCnp()
+					+ " successfully saved!");
+		}
+	}
+
+	private static void printFirstNames() {
+		Person prs = null;
+		prs = prsDAO.getPersonByID(1);
+		System.out.println("The Person with id 1 is: "+ prs.getFirstName());
+		prs = prsDAO.getPersonByID(2);
+		System.out.println("The Person with id 2 is: "+ prs.getFirstName());
+		prs = prsDAO.getPersonByID(3);
+		System.out.println("The Person with id 3 is: "+ prs.getFirstName());
+		prs = prsDAO.getPersonByID(4);
+		System.out.println("The Person with id 4 is: "+ prs.getFirstName());
 	}
 }
